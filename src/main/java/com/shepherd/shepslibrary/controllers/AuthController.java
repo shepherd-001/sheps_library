4735f598cb8c4187cec4a73b8e298b77c3696689
shepherd-001 <shepherdlegend001@gmail.com>
2025-01-08 17:16:33 +0100

    commit

--- src/main/java/com/shepherd/shepslibrary/controllers/AuthController.java
+++ src/main/java/com/shepherd/shepslibrary/controllers/AuthController.java
@@ -1,7 +1,7 @@
 package com.shepherd.shepslibrary.controllers;
 
 import com.shepherd.shepslibrary.controllers.responses.BaseResponse;
-import com.shepherd.shepslibrary.data.dto.request.RegisterUserRequest;
+import com.shepherd.shepslibrary.data.dto.request.*;
 import com.shepherd.shepslibrary.service.auth.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,5 +26,35 @@ public class AuthController {
     public ResponseEntity<Object> verifyEmail(@RequestParam String token){
         return ResponseEntity.ok(BaseResponse
                 .buildResponse(authService.verifyEmail(token)));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@Valid @RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(BaseResponse
+                .buildResponse(authService.login(loginRequest)));
+    }
+
+    @PutMapping("/change-password")
+    public ResponseEntity<Object> changePassword(@Valid @RequestBody ChangePasswordRequest changePasswordRequest) {
+        return ResponseEntity.ok(BaseResponse
+                .buildResponse(authService.changePassword(changePasswordRequest)));
+    }
+
+    @PostMapping("/request-password-reset")
+    public ResponseEntity<Object> requestPasswordReset(@Valid @RequestBody PasswordResetRequest passwordResetRequest) {
+        return ResponseEntity.ok(BaseResponse
+                .buildResponse(authService.requestPasswordReset(passwordResetRequest)));
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<Object> resetPassword(@Valid @RequestBody ResetPasswordRequest resetPasswordRequest) {
+        return ResponseEntity.ok(BaseResponse
+                .buildResponse(authService.resetPassword(resetPasswordRequest)));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Object> logout(){
+        return ResponseEntity.ok(BaseResponse
+                .buildResponse("User logged out successfully"));
     }
 }
