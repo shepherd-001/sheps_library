7ccafb0f37cc7fb8bafaf50e5b680ace8949b70f
shepherd-001 <shepherdlegend001@gmail.com>
2025-01-16 14:15:40 +0100

    feat: configured swagger

--- src/main/java/com/shepherd/shepslibrary/security/AllowedURIs.java
+++ src/main/java/com/shepherd/shepslibrary/security/AllowedURIs.java
@@ -9,7 +9,16 @@ public final class AllowedURIs {
                 "/api/v1/auth/request-password-reset",
                 "/api/v1/auth/reset-password",
                 "/api/v1/librarian/create-password",
-                "/verify"
+                "/v2/api-docs",
+                "/v3/api-docs",
+                "/v3/api-docs/**",
+                "/swagger-resources",
+                "/swagger-resources/**",
+                "/configuration/ui",
+                "/configuration/security",
+                "/swagger-ui/**",
+                "/webjars/**",
+                "/swagger-ui.html"
         };
     }
     private AllowedURIs() {
