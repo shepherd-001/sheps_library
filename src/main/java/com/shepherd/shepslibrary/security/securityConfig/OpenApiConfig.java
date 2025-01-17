4eff9a5879095a1612ac515363bf0b4542687b7f
shepherd-001 <shepherdlegend001@gmail.com>
2025-01-17 16:12:33 +0100

    deploying

--- src/main/java/com/shepherd/shepslibrary/security/securityConfig/OpenApiConfig.java
+++ src/main/java/com/shepherd/shepslibrary/security/securityConfig/OpenApiConfig.java
@@ -33,7 +33,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 ),
                 @Server(
                         description = "PROD ENV",
-                        url = "https://some-url.com"
+                        url = "https://shepslibrary-production.up.railway.app"
                 )
         },
         security = {
--- src/main/java/com/shepherd/shepslibrary/security/securityConfig/SecurityConfig.java
+++ src/main/java/com/shepherd/shepslibrary/security/securityConfig/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
     private final LogoutHandler logoutHandler;
 
     private static final List<String> ALLOWED_ORIGINS = List.of("http://localhost:3000");
+//    private static final List<String> ALLOWED_ORIGINS = List.of("*");
     private static final List<String> ALLOWED_METHODS = List.of("GET", "POST", "DELETE", "PUT", "PATCH");
     private static final List<String> ALLOWED_HEADERS = List.of("Authorization", "Requestor-Type",
             "Origin", "X-Requested-With", "Accept",  "Content-Type", "Cache-Control");
