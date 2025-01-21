27deeaa97109daf39b37a11ba8cb6efc7e0748b1
shepherd-001 <shepherdlegend001@gmail.com>
2025-01-21 14:51:34 +0100

    commit

--- src/main/java/com/shepherd/shepslibrary/data/repository/TransactionRepository.java
+++ src/main/java/com/shepherd/shepslibrary/data/repository/TransactionRepository.java
@@ -4,9 +4,18 @@ import com.shepherd.shepslibrary.data.model.Transaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
     Page<Transaction> findAllByUserId(UUID userId, Pageable pageable);
+
+    @Query("""
+            select t from Transaction t
+            where t.returnDate < :today
+           """)
+    List<Transaction> findOverdueTransactions(LocalDate today);
 }
