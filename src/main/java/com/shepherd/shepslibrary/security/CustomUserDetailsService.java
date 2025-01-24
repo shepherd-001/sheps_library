c3422ea43c085bb332dce890a19ebc839dece43f
shepherd-001 <shepherdlegend001@gmail.com>
2025-01-24 02:16:41 +0100

    refactor: custom authentication provider

--- src/main/java/com/shepherd/shepslibrary/security/CustomUserDetailsService.java
+++ src/main/java/com/shepherd/shepslibrary/security/CustomUserDetailsService.java
@@ -1,8 +1,8 @@
 package com.shepherd.shepslibrary.security;
 
-import com.shepherd.shepslibrary.data.model.User;
 import com.shepherd.shepslibrary.data.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,15 +10,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email).orElseThrow(
-                ()-> new UsernameNotFoundException("User with the provided email not found"));
-        return AuthenticatedUser.builder()
-                .user(user)
-                .build();
+        return userRepository.findByEmail(email)
+                .map(user -> AuthenticatedUser.builder()
+                        .user(user)
+                        .build())
+                .orElseThrow(() -> {
+                    log.info("Login attempt failed: User with email '{}' not found", email);
+                    return new UsernameNotFoundException("User with the provided emil not found");
+                });
     }
 }
