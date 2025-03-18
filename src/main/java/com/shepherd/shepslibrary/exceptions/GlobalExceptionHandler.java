233924713f7d463a16c554d119edfd9385459297
shepherd-001 <shepherdlegend001@gmail.com>
2025-03-18 16:10:36 +0100

    commiting

--- src/main/java/com/shepherd/shepslibrary/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/shepherd/shepslibrary/exceptions/GlobalExceptionHandler.java
@@ -82,7 +82,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AlreadyExistsException.class)
     public ResponseEntity<ApiError> handleException(AlreadyExistsException ex){
         log.error("::::: Already exists exception: {} :::::", ex.getMessage());
-        return new ResponseEntity<>(ApiError.buildErrorResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(ApiError.buildErrorResponse(ex.getMessage()), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(EmailValidationException.class)
